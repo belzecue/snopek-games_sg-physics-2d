25c3045c11399f0beee51d2ac18f20098418d173
David Snopek <dsnopek@gmail.com>
2021-10-18 20:43:40 -0500

    Got the boilerplate for API docs setup

--- godot/modules/sg_physics_2d/config.py
+++ godot/modules/sg_physics_2d/config.py
@@ -3,3 +3,28 @@ def can_build(env, platform):
 
 def configure(env):
     pass
+
+def get_doc_path():
+    return "doc_classes"
+
+def get_doc_classes():
+    return [
+        'SGFixed',
+        'SGFixedVector2',
+        'SGFixedRect2',
+        'SGFixedTransform2D',
+        'SGFixedNode2D',
+        'SGFixedPosition2D',
+        'SGCollisionObject2D',
+        'SGArea2D',
+        'SGStaticBody2D',
+        'SGKinematicBody2D',
+        'SGKinematicCollision2D',
+        'SGRayCast2D',
+        'SGCollisionShape2D',
+        'SGCollisionPolygon2D',
+        'SGShape2D',
+        'SGRectangleShape2D',
+        'SGCircleShape2D',
+    ]
+
